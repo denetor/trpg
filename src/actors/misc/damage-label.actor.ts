2e501d792252e77958a71c6647e615253888a95a
denetor <denetor@gmail.com>
2025-04-24 23:49:26 +0200

    Remove MessageActor and optimize DamageLabel autohide logic
    DamageLabel's auto-hide functionality was improved by replacing setTimeout with a Timer for better integration with the engine's lifecycle.

--- src/actors/misc/damage-label.actor.ts
+++ src/actors/misc/damage-label.actor.ts
@@ -1,11 +1,11 @@
-import {Color, Engine, Font, FontUnit, Label, vec, Vector} from "excalibur";
+import {Color, Engine, Font, FontUnit, Label, Timer, vec, Vector} from "excalibur";
 
 
 /**
  * Label appearing on top of an actor when it receives damage
  */
 export class DamageLabel extends Label {
-
+    private autohideTimer: Timer = undefined as any;
 
 
     constructor(options: {damage: number, pos: Vector}) {
@@ -25,15 +25,21 @@ export class DamageLabel extends Label {
                 },
             })
         });
-        this.vel = vec(0, -15);
     }
 
 
     onInitialize(engine: Engine) {
+        this.vel = vec(0, -15);
         this.z = 200;
-        setTimeout(() => {
-            this.kill();
-        }, 1500);
+        this.autohideTimer = new Timer({
+            interval: 1500,
+            repeats: false,
+            action: () => {
+                this.kill();
+            }
+        });
+        engine.currentScene.add(this.autohideTimer);
+        this.autohideTimer.start();
     }
 
 
